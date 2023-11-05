4b0efec8ac5c751a4addeb8c7b315313f514c7df
tutqq96 <tutqq96@gmail.com>
2023-11-05 14:51:00 +0700

    init

---
+++ ums-service-student/src/main/java/com/uth/ums/student/UmsServiceStudentApplication.java
@@ -0,0 +1,13 @@
+package com.uth.ums.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmsServiceStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmsServiceStudentApplication.class, args);
+	}
+
+}
---
+++ ums-service-student/src/main/java/com/uth/ums/student/service/CareerLevelService.java
@@ -0,0 +1,11 @@
+//package com.uth.ums.career.service;
+//
+//import java.util.List;
+//
+//public interface CareerLevelService {
+//    List<CareerLevelDto> getAll();
+//    CareerLevelDto getById(Long id);
+//    CareerLevelDto createNew(CareerLevelDto dto);
+//    CareerLevelDto update(Long id, CareerLevelDto dto);
+//    void delete(Long id);
+//}
