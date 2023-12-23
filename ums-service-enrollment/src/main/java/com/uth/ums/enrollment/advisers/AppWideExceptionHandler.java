acae5f35bb3b84942b616d096aebe5ebdd511ef1
tutqq96 <tutqq96@gmail.com>
2023-12-23 19:06:10 +0700

    Refactor code

---
+++ ums-service-enrollment/src/main/java/com/uth/ums/enrollment/advisers/AppWideExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.uth.ums.enrollment.advisers;
+
+
+import com.uth.ums.schedule.exception.NotFoundException;
+import com.uth.ums.schedule.exception.ValidateException;
+import com.uth.ums.schedule.util.StandardResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@CrossOrigin
+public class AppWideExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handleException(Exception e) {
+        return new ResponseEntity(new StandardResponse("500", "Error", e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity handleNotFoundException(NotFoundException e) {
+        return new ResponseEntity(new StandardResponse("404", "Error", e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ValidateException.class)
+    public ResponseEntity handleValidationException(ValidateException e) {
+        return new ResponseEntity(new StandardResponse("400", "Error", e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+}
